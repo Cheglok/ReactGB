9c383d05fb40758f458f912c9310829621db47cc
Cheglok <Kopytovmikhail@yandex.ru>
2021-12-12 20:13:55 +0500

    for lesson 7

--- react-gb/src/App.js
+++ react-gb/src/App.js
@@ -5,6 +5,7 @@ import {Messenger} from "./components/Messenger";
 import {Profile} from "./components/Profile";
 import {ChatOne} from "./components/ChatOne";
 import Main from "./components/Main"
+//for lesson7
 
 const Info = () => {
     return (
